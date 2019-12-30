2a60b42dc00061dc3c6041b714cfea14a4a5fc55
RVJonah <barker0184@hotmail.com>
2019-12-30 20:12:52 +0000

    adding environment variables

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import mapper
 
 def initiate_database():
-    db_URI = 'postgres://iwanckcvwprcbs:20ac1c6692687c210f210a1d66be2480d96eda589b2462061d5a962757bfac18@ec2-54-228-207-163.eu-west-1.compute.amazonaws.com:5432/dq638tebaoedu'
+    db_URI = os.environ['DATABASE_URI']
     engine = create_engine(db_URI)
 
     metadata = MetaData()
